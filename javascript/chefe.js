68e2b9548941010da7ce01f1d7791322c87d69ca
Lucas Coelho Barros <lucasbarrosprofessional@gmail.com>
2022-02-17 14:22:04 -0300

    Mudanca na pasta de arquivos e alteracao de codigos

---
+++ javascript/chefe.js
@@ -0,0 +1,58 @@
+'use strict';
+const btnCadastroFuncionario = document.querySelector('div#btnCadastroFuncionario');
+const btnCadastroUsuario = document.querySelector('div#btnCadastroUsuario');
+const btnCloseModalUser = document.querySelector('.close-modal-user');
+const btnsOpenModalUser = document.querySelectorAll('.show-modal-user');
+const btnCloseModalFunc = document.querySelector('.close-modal-func');
+const btnsOpenModalFunc = document.querySelectorAll('.show-modal-func');
+const modalUser = document.querySelector('.modal-user');
+const modalFunc = document.querySelector('.modal-func');
+const overlay = document.querySelector('.overlay');
+
+// FECHANDO AS JANELAS DE CADASTRO
+const closeModalFunc = function() {
+    modalFunc.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+const closeModal = function() {
+    modalUser.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const cadastrarFuncionarios = function(){
+    const openModalFunc = function() {
+        modalFunc.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+    
+    for(let i = 0; i < btnsOpenModalFunc.length; i++)
+        btnsOpenModalFunc[i].addEventListener('click', openModalFunc);
+
+    btnCloseModalFunc.addEventListener('click', closeModalFunc);
+    overlay.addEventListener('click', closeModalFunc);
+
+    document.addEventListener('keydown', function(e) {
+        if(e.key === 'Escape' && !modalFunc.classList.contains('hidden')){
+            closeModalFunc();
+        }
+    });
+}
+const cadastrarUsuarios = function(){
+    const openModal = function() {
+        modalUser.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    for(let i = 0; i < btnsOpenModalUser.length; i++)
+        btnsOpenModalUser[i].addEventListener('click', openModal);
+
+    btnCloseModalUser.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+    document.addEventListener('keydown', function(e) {
+        if(e.key === 'Escape' && !modalUser.classList.contains('hidden')){
+            closeModal();
+        }
+    });
+}
+
---
+++ javascript/login.js
@@ -0,0 +1,94 @@
+//VARIAVÉIS LOGIN
+let listaUser = []
+listaUser.push(
+    {
+        login: "Pedro",
+        senha: "123456",
+        acesso: "FUNCIONARIO"
+    }
+)
+listaUser.push(
+    {
+        login: "Lucas",
+        senha: "000123",
+        acesso: "ADMINISTRADOR"
+    }
+)
+listaUser.push(
+    {
+        login: "Maria",
+        senha: "000456",
+        acesso: "USUARIO"
+    }
+)
+listaUser.push(
+    {
+        login: "Joao",
+        senha: "000789",
+        acesso: "CHEFE"
+    }
+)
+
+//FUNÇÃO VALIDAÇÃO DADOS USUÁRIO
+let validado = (login, senha, acesso) => {
+    for (i = 0; i < listaUser.length; i++) {
+        if (listaUser[i].login == login && listaUser[i].senha == senha && listaUser[i].acesso == acesso) {
+            return true
+        }
+    }
+}
+
+// INICIO FUNÇÃO DO BOTÃO DO FORMULÁRIO DE LOGIN
+function entrar() {
+    //VARIÁVEIS PARA ARMAZENAR INFORMAÇÕES DOS INPUT'S DA TELA DE LOGIN
+    let login = document.getElementById('inputLogin').value
+    let senha = document.getElementById('inputSenha').value
+    let acesso = document.querySelector('input[name="nivelacesso"]:checked').value
+
+    //VALIDAÇÃO CAMPO VAZIO
+    if (login == "" || senha == "") {
+        window.alert("Verifique se os campos foram preenchidos corretamente!")
+    } else {
+        switch (acesso) {
+            case 'FUNCIONARIO':
+                if (validado(login, senha, acesso)) {
+                    alert("Acesso Permitido")
+                    window.alert("Você está logado como " + acesso)
+                    window.location.href = "telainicialfun.html"
+                } else {
+                    alert("Verifique seus dados e tente novamente!")
+                }
+                break
+
+            case 'USUARIO':
+                if (validado(login, senha, acesso)) {
+                    alert("Acesso Permitido")
+                    window.alert("Você está logado como " + acesso)
+                    window.location.href = "telainicialuser.html"
+                } else {
+                    alert("Verifique seus dados e tente novamente!")
+                }
+                break
+
+            case 'ADMINISTRADOR':
+                if (validado(login, senha, acesso)) {
+                    alert("Acesso Permitido")
+                    window.alert("Você está logado como " + acesso)
+                    window.location.href = "telainicialadm.html"
+                } else {
+                    alert("Verifique seus dados e tente novamente!")
+                }
+                break
+
+            case 'CHEFE':
+                if (validado(login, senha, acesso)) {
+                    alert("Acesso Permitido")
+                    window.alert("Você está logado como " + acesso)
+                    window.location.href = "telainicialchefe.html"
+                } else {
+                    alert("Verifique seus dados e tente novamente!")
+                }
+                break
+        }
+    }
+}
